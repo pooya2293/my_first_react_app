d4f962ca54086c2256f56322e3c3f73979214d69
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-22 17:18:29 +0430

    change my first hello world to defult react app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<div className='App'>
+			<h1>Hello From react</h1>
+			<h2>hello</h2>
+		</div>
+	)
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element,document.getElementById('root'))
+// const element = <h1>Hello World</h1>;
+// ReactDOM.render(element,document.getElementById('root'))
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+	, document.getElementById('root')
+	);
