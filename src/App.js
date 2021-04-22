38cecf2a701c29fdcffeb7b4b8e5ebf68d9b8925
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-22 17:35:47 +0430

    install es7 react/redux... plugin and creat Header component and add rafce from es7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 function App() {
-	const name = 'pouriya'
-	const x = false
 	return (
 		<div className='App'>
 			<h1>Hello From react</h1>
-			<h2>hello {name} 	&#160;	&#160;{x?'yes':'no'}</h2>
 		</div>
 	)
 }
