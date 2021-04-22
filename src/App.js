6012407c45f107a2cd817d64f9d533efbd2fbfc4
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-22 18:09:07 +0430

    add class for contact with DOM just for test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react'
 import Header from './components/Header'
-function App() {
-	return (
-		<div className='App'>
-			<Header />
-			<h1>Hello From react</h1>
-		</div>
-	)
+// function App() {
+// 	return (
+// 		<div className='App'>
+// 			<Header />
+// 			<h1>Hello From react</h1>
+// 		</div>
+// 	)
+// }
+
+class App extends React.Component {
+	render() {
+		// return <h1>Hello from a class</h1>
+		return <Header />
+	}
 }
 
 export default App
