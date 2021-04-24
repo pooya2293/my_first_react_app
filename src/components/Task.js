92db7e6cd59292b84c22d32bcaa5556a60eee469
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-24 12:35:06 +0430

    import font-awesome from react-icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,8 @@
+import { FaTimes } from 'react-icons/fa'
 const Task = ({ tsk }) => {
 	return (
 		<div className='task'>
-			<h3>{tsk.text}</h3>
+			<h3>{tsk.text} <FaTimes /> </h3>
 			<h3>{tsk.day}</h3>
 		</div>
 	)
