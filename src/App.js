25c2129a3969f462e8d968022409792f294759a0
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-24 11:37:16 +0430

    add path for tasks and useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Header from './components/Header'
 import MyTasks from './components/MyTasks'
+import { useState } from 'react'
 function App() {
+const [tasks, setTasks] = useState([
+	{
+		id:1,
+		text:'move body',
+		day:'00/01/28 at 08:00am',
+		reminder: true,
+	},
+	{
+		id:2,
+		text:'Reading chef Book',
+		day:'00/01/31 at 12:00am',
+		reminder: false,
+	},
+	{
+		id:3,
+		text:'Buy a T-shirt',
+		day:'00/02/05 at 08:00am',
+		reminder: true,
+	}
+])
+
 	return (
 		<div className='container'>
 			<Header />
-			<MyTasks />
+			<MyTasks tasks={tasks} />
 		</div>
 	)
 }
--- src/components/MyTasks.js
+++ src/components/MyTasks.js
@@ -1,25 +1,4 @@
-import { useState } from 'react'
-const MyTasks = () => {
-	const [tasks, setTasks] = useState([
-	{
-		id:1,
-		text:'move body',
-		day:'00/01/28 at 08:00am',
-		reminder: true,
-	},
-	{
-		id:2,
-		text:'Reading chef Book',
-		day:'00/01/31 at 12:00am',
-		reminder: false,
-	},
-	{
-		id:3,
-		text:'Buy a T-shirt',
-		day:'00/02/05 at 08:00am',
-		reminder: true,
-	}
-])
+const MyTasks = ({ tasks }) => {
 	return (
 		<>
 			{tasks.map((n)=>(
